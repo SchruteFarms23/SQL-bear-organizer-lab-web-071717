c9394d2df6d610421d33e8fbcca2215a55200add
SchruteFarms23 <romanmullady@gmail.com>
2017-07-24 18:12:01 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Mr. Chocolate",23,"M","brown","silly", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Rowdy",21,"M","brown","stupid", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Tabitha",27,"F","white","silly", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Seargeant Brown",23,"M","brown","asshole", 0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Melissa",35,"F","white","flirtatious", 0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Grinch",23,"M","brown","silly", 0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Wendy",18,"F","brown","wild", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (NULL,23,"M","brown","scumbag", 0);
